74f1b88bfbb4a0b8d95cf32de2f7e16b5c2e1505
Alberto Mejia <albertomejia295@gmail.com>
2021-02-26 06:28:35 -0500

    Add files via upload

--- census.py
+++ census.py
@@ -21,16 +21,15 @@ def arrangingRules(rules):
         None
     """
     df = pd.read_csv('http://s3.amazonaws.com/istreet-questions-us-east-1/443606/census.csv', header = None)
-    df = df.iloc[0:10]
-    frequencies = frequency(df)
-    
-    total_records = len(df)
+    # df = df.iloc[0:10]
 
+    _, dataset_supports = apriori(df.to_numpy(), 0.3)
+
+    total_records = len(df)
     confidence_rules_set = {}
 
     for rule in rules:
         # Extracts both rules X & Y from the form {X}=>{Y}.
-
         X_Y = re.search(r"{(.*?)}=>{(.*?)}", rule).groups()
         
         X = tuple(X_Y[0].split(",")) # set of attributes
@@ -40,168 +39,97 @@ def arrangingRules(rules):
         # Confidence(X=>Y): Support(X U Y) / Support(X)
 
         XY = X + (Y, )
-        freq_xy = frequencies[XY] 
-        freq_x = frequencies[X]
-
-        support_xy = freq_xy/total_records
-        support_x = freq_x/total_records
+        support_xy = dataset_supports[frozenset(XY)] 
+        support_x = dataset_supports[frozenset(X)] 
 
         rule_confidence = support_xy/support_x
+        confidence_rules_set[rule] = rule_confidence 
 
-
-        
-        print("Freq XY & X: ", freq_xy, freq_x)
-        print("Support of XY & X: ", support_xy, support_x)
+        # print("Support of XY & X: ", support_xy, support_x)
         print("Confidence: ", rule_confidence)
 
         confidence_rules_set[rule] = rule_confidence 
         # print(XY)
 
-    print(confidence_rules_set)
+    # print(confidence_rules_set)
     return sorted(confidence_rules_set, key=confidence_rules_set.get, reverse=True)
 
-
-# def support(A, n, freqs):
-
-
-
-def get_item_pairs(data, r = 2):
-    """
-    Returns a generator that yields item pairs, one at a time
-
-    Parameters
-    ----------
-    data: iter(iter)
-        A 2D iterable
-
-    Returns
-    -------
-    Generator[(str, str), None, None] 
-        A generator of 2 element tuples, format: Generator[YieldType, SendType, ReturnType]
-
-    Raises
-    ------
-        None
-    """
-    # order_item = order_item.to_numpy()    
-
-    #for dataframe
-    for row in data.itertuples(index=False):
-        for item_pair in combinations(row, r):
-            yield item_pair        
-
-def get_pairs(data, r = 2):
-    for item_pair in combinations(data, r):
-        # print(item_pair)
-        yield item_pair        
-
-
-def combinations_by_subset(seq, r):
-    if r:
-        for i in range(r - 1, len(seq)):
-            for cl in combinations_by_subset(seq[:i], r - 1):
-                yield cl + (seq[i],)
-    else:
-        yield tuple()
-
-def get_combo(data):
-    res = Counter()
-    for row in data.itertuples(index=False):
-        for i in range(2, len(row)+1):
-            # new_row = Counter(combinations_by_subset(row, i))
-            new_row = Counter(get_pairs(row, i))
-            res += new_row
-    return res
-
-def get_combinations(data):
-    attr_counts = Counter()
-    for row in data.itertuples(index=False):
-        for i in range(2, 4): #len(row)+1
-            for item_pair in combinations(row, i):
-                print("PAIR", item_pair)
-                attr_counts[item_pair] += 1
-    return attr_counts
-
-def get_combinations2(data):
-    for row in data.itertuples(index=False):
-        for i in range(2, len(row)+1):
-            for item_pair in combinations(row, i):
-                yield item_pair   
-
-def get_combinations3(data):
-    for row in data.itertuples(index=False):
-        n = len(row)
-        return chain.from_iterable(combinations(row, r) for r in range(2, n+1))
  
-# Returns frequency counts for items and item pairs
-def frequency(iterable):
-    # attribute_pairs = get_item_pairs(iterable)
-    # attribute_pairs = get_combinations(iterable)
-    # attribute_pairs = list(attribute_pairs)
-    # attribute_pairs = Counter(chain.from_iterable(get_combinations2(iterable)))
-    #print(type(attribute_pairs), type(attribute_pairs[0]))
-
-    attribute_pairs = get_combo(iterable)
-    return attribute_pairs
-    # return pd.Series(attribute_pairs).rename("freq")
-
-    # Counter iterates through our attribute pairs and keeps a tally of their occurrence
-    attr_pair_count = Counter(attribute_pairs) # sort indicies? PerformanceWarning from get_combinations
-
-    # print(attr_pair_count)
-
-    return pd.Series(attr_pair_count).rename("freq")
-    # df = pd.DataFrame.from_dict(d, orient='index').reset_index()
-
-# Returns frequency and support associated with item
-def merge_item_stats(item_pairs, item_stats):
-    return (item_pairs
-                .merge(item_stats.rename(columns={'freq': 'freqA', 'support': 'supportA'}), left_on='item_A', right_index=True)
-                .merge(item_stats.rename(columns={'freq': 'freqB', 'support': 'supportB'}), left_on='item_B', right_index=True))
-
-# Returns name associated with item
-def merge_item_name(rules, item_name):
-    columns = ['itemA','itemB','freqAB','supportAB','freqA','supportA','freqB','supportB', 
-               'confidenceAtoB','confidenceBtoA','lift']
-    rules = (rules
-                .merge(item_name.rename(columns={'item_name': 'itemA'}), left_on='item_A', right_on='item_id')
-                .merge(item_name.rename(columns={'item_name': 'itemB'}), left_on='item_B', right_on='item_id'))
-    return rules[columns]     
+def find_individual_attributes(dataset):
+    # finds attribute sets
+    attributes = set()
+    for row in dataset:
+        for attr_val in row:
+            if (attr_val,) not in attributes:
+                attributes.add((attr_val,))
+    list(attributes)
+    result = [frozenset(i) for i in attributes]
+    return result
 
 
-def associationRules(data, min_support):
-    print("Starting data: {:22d}".format(len(data)))
+def filter_set(dataset, attribute_sets, min_support):
+    frequencies = Counter()
+    dataset = [set(row) for row in dataset] # Speed up subset check
+    total_records = len(dataset)
+    for row in dataset:
+        for cur_set in attribute_sets:
+            # If our current set is a subset in our row 
+            if cur_set.issubset(row):
+                # Record frequency of attribute=value set
+                frequencies[cur_set] += 1
+    attribute_supports = {}
+    reduced_attributes = [] # List of our attributes post pruning
 
-    # Calculate item frequency and support
-    item_stats             = frequency(data).to_frame("freq")
-    item_stats['support']  = item_stats['freq'] / len(data) 
+    for attr_set in frequencies:
+        support_of_attr = frequencies[attr_set]/total_records
+        # Check support meets minimum requirements, 
+        if support_of_attr >= min_support:
+            # Record support in dictionary and corresponding set in list
+            attribute_supports[attr_set] = support_of_attr
+            reduced_attributes.append(attr_set)
 
-    # print(item_stats)
-
-    # Filter from data items below min support 
-    qualifying_items       = item_stats[item_stats['support'] >= min_support].index
-    df             = data[data.isin(qualifying_items)]
-
-    # filtered = data['support'] >= min_support
-    # print(item_stats)
-    print(df)
+    return attribute_supports, reduced_attributes
 
 
+def create_combinations(attribute_list, val_idx):
+    new_sets = []
+    # Compare every attribute with every other attribute
+    for i in range(len(attribute_list)):
+        for j in range(i+1, len(attribute_list)):
+            cur_attr, other_attr = list(attribute_list[i]), list(attribute_list[j])
+            # Sort to get head of attributes
+            cur_attr.sort()
+            other_attr.sort()
+            # Use val_idx to find head of attributes
+            cur_attr, other_attr = cur_attr[:val_idx-2], other_attr[:val_idx-2]
 
+            if cur_attr ==  other_attr:
+                # Keep recurring attribute sets
+                combined_set = attribute_list[i].union(attribute_list[j])
+                new_sets.append(combined_set)
+    return new_sets
+
+def apriori(dataset, min_support=0.3):
+    size_1_sets = find_individual_attributes(dataset)
+    # 1st_pruned, support
+    attr_supports, pruned_attrs = filter_set(dataset, size_1_sets, min_support) 
+    attr_list = [pruned_attrs]
+    valid_attrs_index = 2
+
+    # Continue increasing set sizes while pruning away attributes below min support
+    notempty = len(attr_list[valid_attrs_index-2])
+    while (notempty): # Continue until we reach an empty attribute set
+        potential_attr_sets = create_combinations(attr_list[valid_attrs_index-2], valid_attrs_index)
+        new_supports, new_pruned = filter_set(dataset, potential_attr_sets, min_support) 
+        attr_supports.update(new_supports)
+        attr_list.append(new_pruned)
+        valid_attrs_index += 1
+        notempty = len(attr_list[valid_attrs_index-2])
+    return attr_list, attr_supports
 
 if __name__ == '__main__':
-    df = pd.read_csv('http://s3.amazonaws.com/istreet-questions-us-east-1/443606/census.csv', header = None)
-    
-    # df = df.to_numpy()[0:10]
-    df = df.iloc[0:10]
-    # print(df)
-
-    # # Convert from DataFrame to a Series, with order_id as index and item_id as value
-    # orders = orders.set_index('key')['product_id'].rename('item_id')
-
     check1 = ('capital-gain=None', 'capital-loss=None')
     check2 = ('native-country=United-States', 'capital-gain=None', 'capital-loss=None')
-
     rule1 = "{native-country=United-States,capital-gain=None}=>{capital-loss=None}"
     rule2 = "{capital-gain=None,capital-loss=None}=>{native-country=United-States}"
     rule3 = "{native-country=United-States,capital-loss=None}=>{capital-gain=None}"
@@ -215,9 +143,10 @@ if __name__ == '__main__':
     # print(len(x))
     # x.to_csv("result")
 
-    # for i, row in x.iteritems():
-    #     print(i, row) 
+    # for i, row in df.iteritems():
+    #     print(i, row.index) 
 
+    # print(list(df.columns.values))
     # arrangingRules(rules)
     # print(associationRules(df, 0.3))
 
